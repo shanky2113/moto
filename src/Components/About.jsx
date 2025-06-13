c4c534b89e6332d08b47387fa70aac96556a1e17
Shashank rathore <shashankshank13@gmail.com>
2025-06-13 15:10:46 +0530

    changes in about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,29 +1,11 @@
-import React from 'react';
-import '../Components/About.css';
+import React from 'react'
 
-const logos = [
-  '/logos/logo1.svg',
-  '/logos/logo2.png',
-  '/logos/logo3.png',
-  '/logos/logo4.png',
-  '/logos/logo5.png',
-];
-
-const About = () => {
+function About() {
   return (
-    <div className="hero-bottom-center">
-      <h2 className="about-heading">Our Trusted Partners</h2>
-      <div className="logo-slider-container">
-        <div className="logo-slider-box">
-          <div className="logo-slider-track">
-            {[...logos, ...logos].map((src, idx) => (
-              <img key={idx} src={src} alt={`logos${idx}`} className="logo-img" />
-            ))}
-          </div>
-        </div>
-      </div>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
